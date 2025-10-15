ec296245d4162abb0c6872a9124f799dfc2bd59e
xiaowan <33517949+xiaowan108@users.noreply.github.com>
2025-10-15 17:07:28 +0800

    change to correct options for fuse, please remeber to enable user_allow_other in /etc/fuse.conf

--- pCloudCC/lib/pclsync/pfs.c
+++ pCloudCC/lib/pclsync/pfs.c
@@ -3422,7 +3422,7 @@ static int psync_fs_do_start(){
   }
   fuse_opt_add_arg(&args, "-ohard_remove");
 	//允許其他用戶寫入，防止其他程式無法寫入
-	 fuse_opt_add_arg(&args, "-oallow_others");
+	 fuse_opt_add_arg(&args, "-oallow_other");
 //  fuse_opt_add_arg(&args, "-d");
 #endif
 
